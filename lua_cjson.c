551d010cfde4cb16025ff187ec3a0e20a9861516
paynechu <314176+paynechu@users.noreply.github.com>
2013-11-04 17:09:28 +0800

    - supported to use __tostring for encode table if found in the metatable

--- lua_cjson.c
+++ lua_cjson.c
@@ -655,40 +655,48 @@ static void json_append_object(lua_State *l, json_config_t *cfg,
 {
     int comma, keytype;
 
-    /* Object */
-    strbuf_append_char(json, '{');
+    if (luaL_getmetafield(l, -1, "__tostring")) {
+        int top = lua_gettop(l);
+        lua_pushvalue(l, -2);
+        lua_call(l, 1, 1);
+        json_append_string(l, json, -1);
+        lua_settop(l, top - 1);
+    } else {
+        /* Object */
+        strbuf_append_char(json, '{');
 
-    lua_pushnil(l);
-    /* table, startkey */
-    comma = 0;
-    while (lua_next(l, -2) != 0) {
-        if (comma)
-            strbuf_append_char(json, ',');
-        else
-            comma = 1;
+        lua_pushnil(l);
+        /* table, startkey */
+        comma = 0;
+        while (lua_next(l, -2) != 0) {
+            if (comma)
+                strbuf_append_char(json, ',');
+            else
+                comma = 1;
 
-        /* table, key, value */
-        keytype = lua_type(l, -2);
-        if (keytype == LUA_TNUMBER) {
-            strbuf_append_char(json, '"');
-            json_append_number(l, cfg, json, -2);
-            strbuf_append_mem(json, "\":", 2);
-        } else if (keytype == LUA_TSTRING) {
-            json_append_string(l, json, -2);
-            strbuf_append_char(json, ':');
-        } else {
-            json_encode_exception(l, cfg, json, -2,
-                                  "table key must be a number or string");
-            /* never returns */
+            /* table, key, value */
+            keytype = lua_type(l, -2);
+            if (keytype == LUA_TNUMBER) {
+                strbuf_append_char(json, '"');
+                json_append_number(l, cfg, json, -2);
+                strbuf_append_mem(json, "\":", 2);
+            } else if (keytype == LUA_TSTRING) {
+                json_append_string(l, json, -2);
+                strbuf_append_char(json, ':');
+            } else {
+                json_encode_exception(l, cfg, json, -2,
+                                      "table key must be a number or string");
+                /* never returns */
+            }
+
+            /* table, key, value */
+            json_append_data(l, cfg, current_depth, json);
+            lua_pop(l, 1);
+            /* table, key */
         }
 
-        /* table, key, value */
-        json_append_data(l, cfg, current_depth, json);
-        lua_pop(l, 1);
-        /* table, key */
+        strbuf_append_char(json, '}');
     }
-
-    strbuf_append_char(json, '}');
 }
 
 /* Serialise Lua data into JSON string. */
